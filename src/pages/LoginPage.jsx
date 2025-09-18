70f70dd659e058f75542d7b2e142f69ce772ffb5
Nandalily <nankundalilian5@gmail.com>
2025-09-18 08:55:28 -0400

    aaa

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import { Header } from "../components/Header"
-import { Footer } from "../components/Footer"
+// import { Footer } from "../components/Footer"
 import { Button } from "../components/ui/button"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
@@ -211,7 +211,7 @@ export default function LoginPage() {
           </div>
         </div>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
